0ac1bb1e6a9af44525d17800d51c71282b8eca66
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 23:31:31 +0700

    Restore working WESTSIDE update feed backup

--- updates/heads/fivereborn/production.php
+++ updates/heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
 header('x-amz-meta-bootstrap-version: 1000015');
-header('x-amz-meta-bootstrap-size: 5293596');
-header('x-amz-meta-bootstrap-object: 23de0c6ee43b6a71bd56e0b9f671b54ac4704ab5e2a35be67a1f12d15a158f16');
-header('x-amz-meta-branch-version: 1778689667');
-header('x-amz-meta-branch-manifest: fe7ee86417784a39db37f41d31328d646148fe0718225b3f835b9fdbb63912be');
+header('x-amz-meta-bootstrap-size: 8676352');
+header('x-amz-meta-bootstrap-object: 45ae9b30896b7ea28781c92dc0ef54e60c16e4b47f194c0612730e72513c78dc');
+header('x-amz-meta-branch-version: 1778678112');
+header('x-amz-meta-branch-manifest: 8f154b293fe516e8ef2c725a801bafa68d7f3bd130a585dab6e39fcc28d47b92');
 echo "OK";
 ?>
