0eccb71ee0535fd177482313cc0e6b3d2a8f6ac7
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 10:49:49 +0700

    Restore WESTSIDE updater 1000015 payload

---
+++ updates/heads/fivereborn/production.php
@@ -0,0 +1,8 @@
+<?php
+header('x-amz-meta-bootstrap-version: 1000015');
+header('x-amz-meta-bootstrap-size: 5293564');
+header('x-amz-meta-bootstrap-object: b4f80e4684cc03275ae58225e4ebb9c0204090844ba3738a4a0c2126d03336a5');
+header('x-amz-meta-branch-version: 1778644097');
+header('x-amz-meta-branch-manifest: 30bda4d1ab94259b4f152c6c46bd61483334850ff027b0755226bb09ef22a3f1');
+echo "OK";
+?>
