e2fd43ff3d0af3291566fdb9c98611a5f410a8c9
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 12:01:08 +0700

    Publish WESTSIDE Discord recovery update

--- updates/heads/fivereborn/production.php
+++ updates/heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000015');
 header('x-amz-meta-bootstrap-size: 5293564');
 header('x-amz-meta-bootstrap-object: b4f80e4684cc03275ae58225e4ebb9c0204090844ba3738a4a0c2126d03336a5');
-header('x-amz-meta-branch-version: 1778645301');
-header('x-amz-meta-branch-manifest: b7c01f3acb3242fd62b827a4dee707544d1f2623da3b62986e491471eb33813a');
+header('x-amz-meta-branch-version: 1778648455');
+header('x-amz-meta-branch-manifest: c59dde0592ea4848ee6825f4bed3d8c83f9165c39151726b2d14b06680f00cca');
 echo "OK";
 ?>
