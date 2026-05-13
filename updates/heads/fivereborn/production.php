3be0f8c4b092f3b0323652f79873c613a7d35c09
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 22:52:32 +0700

    Add legitimacy component to WESTSIDE package

--- updates/heads/fivereborn/production.php
+++ updates/heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000015');
 header('x-amz-meta-bootstrap-size: 5293596');
 header('x-amz-meta-bootstrap-object: 23de0c6ee43b6a71bd56e0b9f671b54ac4704ab5e2a35be67a1f12d15a158f16');
-header('x-amz-meta-branch-version: 1778682079');
-header('x-amz-meta-branch-manifest: eeb5418cd043bbdae779075669538ec18b2bdc74b008fc288a160a3fec55a9d0');
+header('x-amz-meta-branch-version: 1778687531');
+header('x-amz-meta-branch-manifest: 6a24f5efb081816637e790bdad4a30a9aa6fe108d08509b9b8171c8b2aa57ba2');
 echo "OK";
 ?>
