9a3c6310a6c5f625122680d59b63bb59756aec40
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 19:24:23 +0700

    Publish component proxy manifest fix

--- updates/heads/fivereborn/production.php
+++ updates/heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000015');
 header('x-amz-meta-bootstrap-size: 8676352');
 header('x-amz-meta-bootstrap-object: 45ae9b30896b7ea28781c92dc0ef54e60c16e4b47f194c0612730e72513c78dc');
-header('x-amz-meta-branch-version: 1778674360');
-header('x-amz-meta-branch-manifest: 850298ccd303c2b227fc99b87c6fea0309b24166c564a236a250e481e02adce1');
+header('x-amz-meta-branch-version: 1778674935');
+header('x-amz-meta-branch-manifest: 97b67d897b94dfb272d6b590acfe21fa997f33609a1d796dc70cc73c14301346');
 echo "OK";
 ?>
