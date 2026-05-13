0bad53f14592af82e94b48a155733cd2d670bfb9
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 17:30:08 +0700

    Load legitimacy component before adhesive

--- updates/heads/fivereborn/production.php
+++ updates/heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000021');
 header('x-amz-meta-bootstrap-size: 8676352');
 header('x-amz-meta-bootstrap-object: 45ae9b30896b7ea28781c92dc0ef54e60c16e4b47f194c0612730e72513c78dc');
-header('x-amz-meta-branch-version: 1778667610');
-header('x-amz-meta-branch-manifest: 848cf64b42d7ac5560157185aed5544b101bbd060a18990386cac59e72740d2f');
+header('x-amz-meta-branch-version: 1778668198');
+header('x-amz-meta-branch-manifest: 1f94e9a7a910fda61395bd35d0a09b3e37487ddec02b80f060d76f73389ff0a7');
 echo "OK";
 ?>
