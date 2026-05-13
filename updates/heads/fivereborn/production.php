8c1044571a9c7a5948279963a7cf406df71d060c
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 20:15:32 +0700

    Load sticky game process component

--- updates/heads/fivereborn/production.php
+++ updates/heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000015');
 header('x-amz-meta-bootstrap-size: 8676352');
 header('x-amz-meta-bootstrap-object: 45ae9b30896b7ea28781c92dc0ef54e60c16e4b47f194c0612730e72513c78dc');
-header('x-amz-meta-branch-version: 1778676757');
-header('x-amz-meta-branch-manifest: 18596905a8aaa895989228320125ce14e84c7ed0fd43f8349096b70c705228e9');
+header('x-amz-meta-branch-version: 1778678112');
+header('x-amz-meta-branch-manifest: 8f154b293fe516e8ef2c725a801bafa68d7f3bd130a585dab6e39fcc28d47b92');
 echo "OK";
 ?>
