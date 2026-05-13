0794d599b219dcfe38daa75fcd3d3e17f8839fc2
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 18:34:50 +0700

    Restore legitimacy component in update manifest

--- updates/heads/fivereborn/production.php
+++ updates/heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000015');
 header('x-amz-meta-bootstrap-size: 8676352');
 header('x-amz-meta-bootstrap-object: 45ae9b30896b7ea28781c92dc0ef54e60c16e4b47f194c0612730e72513c78dc');
-header('x-amz-meta-branch-version: 1778671806');
-header('x-amz-meta-branch-manifest: 615c159cec1c19c01d26d0d1e1a6f554e7d326f7e5c272cd16d2679f9be0d743');
+header('x-amz-meta-branch-version: 1778672065');
+header('x-amz-meta-branch-manifest: 084fd775674b719190c43e475507ab8d0d34adaeea94d349bba975f62138d77c');
 echo "OK";
 ?>
