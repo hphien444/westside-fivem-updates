98f7e8b60d34ae0b152148443aea611116340922
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 16:59:59 +0700

    Remove manifest from legitimacy component

--- updates/heads/fivereborn/production.php
+++ updates/heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000020');
 header('x-amz-meta-bootstrap-size: 8676352');
 header('x-amz-meta-bootstrap-object: 784e9b713d08ca9b10a62255e43179aa146f902b2932667252357c6bdd38d26c');
-header('x-amz-meta-branch-version: 1778666002');
-header('x-amz-meta-branch-manifest: 21ddd685782da90deb0311aa4d02385a589619da4d876bfdbf4716b54c729913');
+header('x-amz-meta-branch-version: 1778666382');
+header('x-amz-meta-branch-manifest: 848cf64b42d7ac5560157185aed5544b101bbd060a18990386cac59e72740d2f');
 echo "OK";
 ?>
