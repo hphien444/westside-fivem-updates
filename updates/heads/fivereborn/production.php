59d0808284b397cb54e1d03c383ff6407bdd7fc9
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 18:30:45 +0700

    Publish clothing addon update

--- updates/heads/fivereborn/production.php
+++ updates/heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
 header('x-amz-meta-bootstrap-version: 1000015');
-header('x-amz-meta-bootstrap-size: 5293564');
-header('x-amz-meta-bootstrap-object: b4f80e4684cc03275ae58225e4ebb9c0204090844ba3738a4a0c2126d03336a5');
-header('x-amz-meta-branch-version: 1778648455');
-header('x-amz-meta-branch-manifest: c59dde0592ea4848ee6825f4bed3d8c83f9165c39151726b2d14b06680f00cca');
+header('x-amz-meta-bootstrap-size: 8676352');
+header('x-amz-meta-bootstrap-object: 45ae9b30896b7ea28781c92dc0ef54e60c16e4b47f194c0612730e72513c78dc');
+header('x-amz-meta-branch-version: 1778671806');
+header('x-amz-meta-branch-manifest: 615c159cec1c19c01d26d0d1e1a6f554e7d326f7e5c272cd16d2679f9be0d743');
 echo "OK";
 ?>
