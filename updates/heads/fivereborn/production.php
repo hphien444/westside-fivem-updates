8d8edf6e960758be716be9a0a7422b8983afbcfc
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 19:13:05 +0700

    Publish adhesive loader compatibility fix

--- updates/heads/fivereborn/production.php
+++ updates/heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000015');
 header('x-amz-meta-bootstrap-size: 8676352');
 header('x-amz-meta-bootstrap-object: 45ae9b30896b7ea28781c92dc0ef54e60c16e4b47f194c0612730e72513c78dc');
-header('x-amz-meta-branch-version: 1778673083');
-header('x-amz-meta-branch-manifest: 47807910e1d30e8903d81cffe07cce953fa6423c4438b4c68c64c087fe452c14');
+header('x-amz-meta-branch-version: 1778674360');
+header('x-amz-meta-branch-manifest: 850298ccd303c2b227fc99b87c6fea0309b24166c564a236a250e481e02adce1');
 echo "OK";
 ?>
