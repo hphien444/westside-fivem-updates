e13081ed34b7ac71e45996f238ca2ef9423a387f
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 23:06:31 +0700

    Rebuild Steam API proxy without CRT dependency

--- updates/heads/fivereborn/production.php
+++ updates/heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000015');
 header('x-amz-meta-bootstrap-size: 5293596');
 header('x-amz-meta-bootstrap-object: 23de0c6ee43b6a71bd56e0b9f671b54ac4704ab5e2a35be67a1f12d15a158f16');
-header('x-amz-meta-branch-version: 1778688146');
-header('x-amz-meta-branch-manifest: 503a18a65257bcc65a0dc5df90d2ef32fc7c6f2bfffd105cb888a460cca37271');
+header('x-amz-meta-branch-version: 1778688376');
+header('x-amz-meta-branch-manifest: 5cc6fab6f63aa5166c168a98c670c49b6f5db7a92f5d4875184613438a72d1e2');
 echo "OK";
 ?>
