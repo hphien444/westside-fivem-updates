766bfff019b729cad8ea263f401c4c4f828ddf8c
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 17:20:17 +0700

    Publish clean rebuilt bootstrap version

--- updates/heads/fivereborn/production.php
+++ updates/heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
 header('x-amz-meta-bootstrap-version: 1000021');
 header('x-amz-meta-bootstrap-size: 8676352');
-header('x-amz-meta-bootstrap-object: 4139d305b628994804e7bec784ea2d5c08055514f4f5eed9dc4f6694e28ccaef');
-header('x-amz-meta-branch-version: 1778667429');
+header('x-amz-meta-bootstrap-object: 45ae9b30896b7ea28781c92dc0ef54e60c16e4b47f194c0612730e72513c78dc');
+header('x-amz-meta-branch-version: 1778667610');
 header('x-amz-meta-branch-manifest: 848cf64b42d7ac5560157185aed5544b101bbd060a18990386cac59e72740d2f');
 echo "OK";
 ?>
