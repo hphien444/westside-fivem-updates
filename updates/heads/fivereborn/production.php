a34c2b488a5929214b08eef973e2bbe434286f12
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 11:09:23 +0700

    Publish WESTSIDE Discord identity update

--- updates/heads/fivereborn/production.php
+++ updates/heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000015');
 header('x-amz-meta-bootstrap-size: 5293564');
 header('x-amz-meta-bootstrap-object: b4f80e4684cc03275ae58225e4ebb9c0204090844ba3738a4a0c2126d03336a5');
-header('x-amz-meta-branch-version: 1778644097');
-header('x-amz-meta-branch-manifest: 30bda4d1ab94259b4f152c6c46bd61483334850ff027b0755226bb09ef22a3f1');
+header('x-amz-meta-branch-version: 1778645301');
+header('x-amz-meta-branch-manifest: b7c01f3acb3242fd62b827a4dee707544d1f2623da3b62986e491471eb33813a');
 echo "OK";
 ?>
