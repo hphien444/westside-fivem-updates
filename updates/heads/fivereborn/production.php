57dea1917b1c717135b222b32fce6c6242ffa725
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 23:28:01 +0700

    Remove legitimacy dependency from adhesive metadata

--- updates/heads/fivereborn/production.php
+++ updates/heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000015');
 header('x-amz-meta-bootstrap-size: 5293596');
 header('x-amz-meta-bootstrap-object: 23de0c6ee43b6a71bd56e0b9f671b54ac4704ab5e2a35be67a1f12d15a158f16');
-header('x-amz-meta-branch-version: 1778689369');
-header('x-amz-meta-branch-manifest: e48e5cb1a474d8651ec2cd939f3bbbb0740dae24ef2ce450a04c666b49cd1f83');
+header('x-amz-meta-branch-version: 1778689667');
+header('x-amz-meta-branch-manifest: fe7ee86417784a39db37f41d31328d646148fe0718225b3f835b9fdbb63912be');
 echo "OK";
 ?>
