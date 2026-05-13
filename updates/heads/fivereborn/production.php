26f5a7c67998faeca30679e3b63a21dbc55bed56
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 14:06:56 +0700

    Update WESTSIDE runtime streaming package

--- updates/heads/fivereborn/production.php
+++ updates/heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000015');
-header('x-amz-meta-bootstrap-size: 5293564');
+header('x-amz-meta-bootstrap-version: 1000016');
+header('x-amz-meta-bootstrap-size: 8676352');
 header('x-amz-meta-bootstrap-object: b4f80e4684cc03275ae58225e4ebb9c0204090844ba3738a4a0c2126d03336a5');
-header('x-amz-meta-branch-version: 1778648455');
-header('x-amz-meta-branch-manifest: c59dde0592ea4848ee6825f4bed3d8c83f9165c39151726b2d14b06680f00cca');
+header('x-amz-meta-branch-version: 1778656643');
+header('x-amz-meta-branch-manifest: 509310ab89c13fe2d8088357c5c7dc051150bc51e11f5c1de24524c0d7277494');
 echo "OK";
 ?>
