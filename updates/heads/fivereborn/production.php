eeb6c3998ccd8e3a2b01af18fc6cfbded2330463
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 10:49:13 +0700

    Publish WESTSIDE updater 1000015

--- updates/heads/fivereborn/production.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('x-amz-meta-bootstrap-version: 1000014');
-header('x-amz-meta-bootstrap-size: 5293532');
-header('x-amz-meta-bootstrap-object: e1cd29354059cc8f582a183be4b97d49e4234aad9f58343abde0b055867265e7');
-header('x-amz-meta-branch-version: 1778641674');
-header('x-amz-meta-branch-manifest: 30bda4d1ab94259b4f152c6c46bd61483334850ff027b0755226bb09ef22a3f1');
-echo "OK";
-?>
