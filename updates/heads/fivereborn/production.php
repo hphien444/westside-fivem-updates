2e4bc3a4b8b3d04fefde637bb35d0f5e3f21ee3a
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 10:09:03 +0700

    Publish WESTSIDE native identity update

--- updates/heads/fivereborn/production.php
+++ updates/heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000014');
 header('x-amz-meta-bootstrap-size: 5293532');
 header('x-amz-meta-bootstrap-object: e1cd29354059cc8f582a183be4b97d49e4234aad9f58343abde0b055867265e7');
-header('x-amz-meta-branch-version: 1778641013');
-header('x-amz-meta-branch-manifest: 2ddef9c8b87d443f26f6185ed58f19dcccf664e502f58a105cd8c2ff70e44504');
+header('x-amz-meta-branch-version: 1778641674');
+header('x-amz-meta-branch-manifest: 30bda4d1ab94259b4f152c6c46bd61483334850ff027b0755226bb09ef22a3f1');
 echo "OK";
 ?>
