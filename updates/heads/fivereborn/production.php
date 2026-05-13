0e13c4f832ea200ae25a679590dc4b59ab83e9cc
hphien444 <hphien444@users.noreply.github.com>
2026-05-13 16:53:28 +0700

    Restore raw GitHub updater endpoint

--- updates/heads/fivereborn/production.php
+++ updates/heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000019');
+header('x-amz-meta-bootstrap-version: 1000020');
 header('x-amz-meta-bootstrap-size: 8676352');
-header('x-amz-meta-bootstrap-object: 67025a40a07eb5aeb6848f71736a143b04e09b4dd8a2547730147f3c1af1efe4');
-header('x-amz-meta-branch-version: 1778665791');
+header('x-amz-meta-bootstrap-object: 784e9b713d08ca9b10a62255e43179aa146f902b2932667252357c6bdd38d26c');
+header('x-amz-meta-branch-version: 1778666002');
 header('x-amz-meta-branch-manifest: 21ddd685782da90deb0311aa4d02385a589619da4d876bfdbf4716b54c729913');
 echo "OK";
 ?>
