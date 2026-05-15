82e0d4e4309813e1730a4ba9ced1881297f7617b
HIEN <hien@example.com>
2026-05-15 21:53:21 +0700

    Mini update: fauxrpf retry

--- updates/heads/fivereborn/production.php
+++ updates/heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000015');
-header('x-amz-meta-bootstrap-size: 5292700');
-header('x-amz-meta-bootstrap-object: 990b6d2c4fd460a52d8d0e26f8fbfe53c0773ab9aee2eb0ed05a6f681643d828');
-header('x-amz-meta-branch-version: 1778852364');
-header('x-amz-meta-branch-manifest: e8cf75ec1d5797fa1c05253132e6504f6d7fb1cdc30b6cfc873e24f679c1e024');
+header("x-amz-meta-bootstrap-version: 1000015");
+header("x-amz-meta-bootstrap-size: 5292700");
+header("x-amz-meta-bootstrap-object: 990b6d2c4fd460a52d8d0e26f8fbfe53c0773ab9aee2eb0ed05a6f681643d828");
+header("x-amz-meta-branch-version: 1778856792");
+header("x-amz-meta-branch-manifest: c288655ee8d13eec890adbf88e854904d053eb89199146d3f233c3db72c4fbf2");
 echo "OK";
 ?>
